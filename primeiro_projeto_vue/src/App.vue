9ddd2f7a33f3221148055738fc40622d14725a92
John Robert <john.robert.c.melo@gmail.com>
2024-01-24 01:40:22 -0300

    Entrega de exercício.

---
+++ primeiro_projeto_vue/src/App.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { reactive } from 'vue';
+
+const nome = "Jay"
+
+// let contador = 0
+const estado = reactive({
+  contador: 0,
+})
+
+function incrementar() {
+  estado.contador++;
+}
+function decrementar() {
+  estado.contador--;
+}
+
+</script>
+
+<template>
+  <h1>{{ nome }}</h1>
+  <br />
+  <hr />
+
+  {{ estado.contador }}
+
+  <button @click="incrementar">+</button>
+  <button @click="decrementar">+</button>
+</template>
+
+<style scoped>
+</style>
